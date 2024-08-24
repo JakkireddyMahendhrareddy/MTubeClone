2bf7c871801ecc7c346df9877e7025f674b34613
Jakkireddy Mahendrareddy <jakkireddymahendhrareddy@gmail.com>
2024-08-24 12:33:53 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,41 +1,4 @@
-// import React from 'react'
-// import './Navbar.css'
-// import menu_icon from '../../assets/menu.png'
-// import logo from '../../assets/logo.png'
-// import search_icon from '../../assets/search.png'
-// import upload_icon from '../../assets/upload.png'
-// import more_icon from '../../assets/more.png'
-// import notification_icon from '../../assets/notification.png'
-// import profile_icon from '../../assets/jack.png'
-// import { Link } from 'react-router-dom'
 
-
-// const Navbar = ({setSidebar}) => {
-//   return (
-//     <nav className='flex-div'>
-//         <div className='nav-left flex-div'>
-//             <img src={menu_icon} onClick={()=>setSidebar(prev=>prev===false?true:false)} className='menu-icon' alt=""/>
-//             <Link to='/'><img src={logo} className='logo' alt=""/></Link>
-//         </div>
-//         <div className='nav-middle flex-div'>
-//             <div className="search-box flex-div">
-//             <input type="text" placeholder='Search'/>
-//             <img src={search_icon} alt=""/>
-//             </div>
-//         </div>
-//         <div className="nav-right flex-div">
-//         <img src={upload_icon} className='logo' alt=""/> 
-//         <img src={more_icon} className='logo' alt=""/> 
-//         <img src={notification_icon} className='logo' alt=""/> 
-//         <img src={profile_icon} className='user-icon' alt=""/>
-//         </div>
-
-
-//     </nav>
-//   )
-// }
-
-// export default Navbar
 
 
 import React, { useState } from 'react';
@@ -46,7 +9,7 @@ import search_icon from '../../assets/search.png';
 import upload_icon from '../../assets/upload.png';
 import more_icon from '../../assets/more.png';
 import notification_icon from '../../assets/notification.png';
-import profile_icon from '../../assets/jack.jpg';
+import profile_icon from '../../assets/simon.png';
 import sun_icon from '../../assets/sun.png';
 import moon_icon from '../../assets/moon.png';
 import { Link } from 'react-router-dom';
